4989237968b1a4924a45929e83cb5222d08c4ee0
Keith <suenkaleongkeith@gmail.com>
2019-05-11 17:05:55 +0800

    Win game condition: get the coin

---
+++ Assets/GameOverControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOverControl : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+
+    }
+}
