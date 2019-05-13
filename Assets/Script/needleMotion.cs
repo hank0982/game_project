d63a56b98572de105e6bb3f7ee3653fa7de42965
Keith <suenkaleongkeith@gmail.com>
2019-05-13 23:37:38 +0800

    game over when player hit the traps

--- Assets/Script/needleMotion.cs
+++ Assets/Script/needleMotion.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class needleMotion : MonoBehaviour
 {
@@ -9,13 +10,31 @@ public class needleMotion : MonoBehaviour
 
     private int current = 0;
 
+    void Start()
+    {
+        
+    }
+
     void Update()
     {
         if (transform.position != target[current].position)
         {
             Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
-            GetComponent<Rigidbody>().MovePosition(pos);
+            // GetComponent<Rigidbody>().MovePosition(pos);
+            transform.position = pos;
         }
         else current = (current + 1) % target.Length;
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Debug.Log("hit by traps! : " + other.name + " " + other.tag);
+            PlayerPrefs.SetInt("level", 1);
+            Cursor.lockState = CursorLockMode.None;
+            SceneManager.LoadScene(3);
+        }
+    }
+
 }
