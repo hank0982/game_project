5c17b9b85c345c1423a8d7780a7a7bcea45a6522
Keith <suenkaleongkeith@gmail.com>
2019-05-14 22:34:35 +0800

    loading screen before game level

---
+++ Assets/LoadingControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingControl : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject loadingObj;
+    public Slider slider;
+
+    AsyncOperation async;
+
+    public void LoadingScreen(int level)
+    {
+        StartCoroutine(StartLoading(level));
+    }
+
+    IEnumerator StartLoading(int level)
+    {
+        panel.SetActive(false);
+        loadingObj.SetActive(true);
+        async = SceneManager.LoadSceneAsync(level);
+        async.allowSceneActivation = false;
+
+        while (async.isDone == false)
+        {
+            slider.value = async.progress;
+            if(async.progress == 0.9f)
+            {
+                slider.value = 1f;
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+    
+}
